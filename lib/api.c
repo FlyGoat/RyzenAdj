c9f8e509cc7726abced634d68caa2e2873f342d4
Jiaxun Yang <jiaxun.yang@flygoat.com>
2019-06-03 09:15:59 +0800

    Fix build on Linux
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>

--- lib/api.c
+++ lib/api.c
@@ -4,7 +4,7 @@
 
 #include "ryzenadj.h"
 
-__declspec(dllexport) ryzen_access __stdcall init_ryzenadj(){
+EXP ryzen_access CALL init_ryzenadj(){
 	smu_service_args_t args = {0, 0, 0, 0, 0, 0};
 	ryzen_access ry;
 
@@ -52,7 +52,7 @@ out_free_pci_obj:
 	return NULL;
 }
 
-__declspec(dllexport) void __stdcall cleanup_ryzenadj(ryzen_access ry){
+EXP void CALL cleanup_ryzenadj(ryzen_access ry){
 	free_smu(ry->psmu);
 	free_smu(ry->mp1_smu);
 	free_nb(ry->nb);
@@ -72,90 +72,90 @@ do{ \
 }while(0);
 
 
-__declspec(dllexport) int __stdcall set_stapm_limit(ryzen_access ry, uint32_t value){
+EXP int CALL set_stapm_limit(ryzen_access ry, uint32_t value){
 	_do_adjust(0x1a);
 }
 
-__declspec(dllexport) int __stdcall set_fast_limit(ryzen_access ry, uint32_t value){
+EXP int CALL set_fast_limit(ryzen_access ry, uint32_t value){
 	_do_adjust(0x1b);
 }
 
-__declspec(dllexport) int __stdcall set_slow_limit(ryzen_access ry, uint32_t value){
+EXP int CALL set_slow_limit(ryzen_access ry, uint32_t value){
 	_do_adjust(0x1c);
 }
 
-__declspec(dllexport) int __stdcall set_slow_time(ryzen_access ry, uint32_t value){
+EXP int CALL set_slow_time(ryzen_access ry, uint32_t value){
 	_do_adjust(0x1d);
 }
 
-__declspec(dllexport) int __stdcall set_stapm_time(ryzen_access ry, uint32_t value){
+EXP int CALL set_stapm_time(ryzen_access ry, uint32_t value){
 	_do_adjust(0x1e);
 }
 
-__declspec(dllexport) int __stdcall set_tctl_temp(ryzen_access ry, uint32_t value){
+EXP int CALL set_tctl_temp(ryzen_access ry, uint32_t value){
 	_do_adjust(0x1f);
 }
 
-__declspec(dllexport) int __stdcall set_vrm_current(ryzen_access ry, uint32_t value){
+EXP int CALL set_vrm_current(ryzen_access ry, uint32_t value){
 	_do_adjust(0x20);
 }
 
-__declspec(dllexport) int __stdcall set_vrmsoc_current(ryzen_access ry, uint32_t value){
+EXP int CALL set_vrmsoc_current(ryzen_access ry, uint32_t value){
 	_do_adjust(0x21);
 }
 
-__declspec(dllexport) int __stdcall set_vrmmax_current(ryzen_access ry, uint32_t value){
+EXP int CALL set_vrmmax_current(ryzen_access ry, uint32_t value){
 	_do_adjust(0x22);
 }
 
-__declspec(dllexport) int __stdcall set_vrmsocmax_current(ryzen_access ry, uint32_t value){
+EXP int CALL set_vrmsocmax_current(ryzen_access ry, uint32_t value){
 	_do_adjust(0x23);
 }
 
-__declspec(dllexport) int __stdcall set_psi0_current(ryzen_access ry, uint32_t value){
+EXP int CALL set_psi0_current(ryzen_access ry, uint32_t value){
 	_do_adjust(0x24);
 }
 
-__declspec(dllexport) int __stdcall set_psi0soc_current(ryzen_access ry, uint32_t value){
+EXP int CALL set_psi0soc_current(ryzen_access ry, uint32_t value){
 	_do_adjust(0x25);
 }
 
-__declspec(dllexport) int __stdcall set_max_gfxclk_freq(ryzen_access ry, uint32_t value) {
+EXP int CALL set_max_gfxclk_freq(ryzen_access ry, uint32_t value) {
 	_do_adjust(0x46);
 }
 
-_declspec(dllexport) int __stdcall set_min_gfxclk_freq(ryzen_access ry, uint32_t value) {
+EXP int CALL set_min_gfxclk_freq(ryzen_access ry, uint32_t value) {
 	_do_adjust(0x47);
 }
 
-__declspec(dllexport) int __stdcall set_max_socclk_freq(ryzen_access ry, uint32_t value){
+EXP int CALL set_max_socclk_freq(ryzen_access ry, uint32_t value){
 	_do_adjust(0x48);
 }
 
-__declspec(dllexport) int __stdcall set_min_socclk_freq(ryzen_access ry, uint32_t value){
+EXP int CALL set_min_socclk_freq(ryzen_access ry, uint32_t value){
 	_do_adjust(0x49);
 }
 
-__declspec(dllexport) int __stdcall set_max_fclk_freq(ryzen_access ry, uint32_t value){
+EXP int CALL set_max_fclk_freq(ryzen_access ry, uint32_t value){
 	_do_adjust(0x4A);
 }
 
-__declspec(dllexport) int __stdcall set_min_fclk_freq(ryzen_access ry, uint32_t value){
+EXP int CALL set_min_fclk_freq(ryzen_access ry, uint32_t value){
 	_do_adjust(0x4B);
 }
 
-__declspec(dllexport) int __stdcall set_max_vcn(ryzen_access ry, uint32_t value){
+EXP int CALL set_max_vcn(ryzen_access ry, uint32_t value){
 	_do_adjust(0x4C);
 }
 
-__declspec(dllexport) int __stdcall set_min_vcn(ryzen_access ry, uint32_t value){
+EXP int CALL set_min_vcn(ryzen_access ry, uint32_t value){
 	_do_adjust(0x4D);
 }
 
-__declspec(dllexport) int __stdcall set_max_lclk(ryzen_access ry, uint32_t value){
+EXP int CALL set_max_lclk(ryzen_access ry, uint32_t value){
 	_do_adjust(0x4E);
 }
 
-__declspec(dllexport) int __stdcall set_min_lclk(ryzen_access ry, uint32_t value){
+EXP int CALL set_min_lclk(ryzen_access ry, uint32_t value){
 	_do_adjust(0x4F);
 }
--- lib/ryzenadj.h
+++ lib/ryzenadj.h
@@ -9,6 +9,14 @@ extern "C" {
 #endif
 #include  "nb_smu_ops.h"
 
+#ifdef _WIN32
+#define EXP __declspec(dllexport)
+#define CALL __stdcall
+#else
+#define EXP
+#define CALL
+#endif
+
 #define RYZENADJ_VER 3
 
 typedef struct {
@@ -18,32 +26,32 @@ typedef struct {
 	smu_t psmu;
 } *ryzen_access;
 
-__declspec(dllexport) ryzen_access __stdcall init_ryzenadj();
+EXP ryzen_access CALL init_ryzenadj();
 
-__declspec(dllexport) void __stdcall cleanup_ryzenadj(ryzen_access ry);
+EXP void CALL cleanup_ryzenadj(ryzen_access ry);
 
-__declspec(dllexport) int __stdcall set_stapm_limit(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_fast_limit(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_slow_limit(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_slow_time(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_stapm_time(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_tctl_temp(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_vrm_current(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_vrmsoc_current(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_vrmmax_current(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_vrmsocmax_current(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_psi0_current(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_psi0soc_current(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_max_gfxclk_freq(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_min_gfxclk_freq(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_max_socclk_freq(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_min_socclk_freq(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_max_fclk_freq(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_min_fclk_freq(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_max_vcn(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_min_vcn(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_max_lclk(ryzen_access, uint32_t value);
-__declspec(dllexport) int __stdcall set_min_lclk(ryzen_access, uint32_t value);
+EXP int CALL set_stapm_limit(ryzen_access, uint32_t value);
+EXP int CALL set_fast_limit(ryzen_access, uint32_t value);
+EXP int CALL set_slow_limit(ryzen_access, uint32_t value);
+EXP int CALL set_slow_time(ryzen_access, uint32_t value);
+EXP int CALL set_stapm_time(ryzen_access, uint32_t value);
+EXP int CALL set_tctl_temp(ryzen_access, uint32_t value);
+EXP int CALL set_vrm_current(ryzen_access, uint32_t value);
+EXP int CALL set_vrmsoc_current(ryzen_access, uint32_t value);
+EXP int CALL set_vrmmax_current(ryzen_access, uint32_t value);
+EXP int CALL set_vrmsocmax_current(ryzen_access, uint32_t value);
+EXP int CALL set_psi0_current(ryzen_access, uint32_t value);
+EXP int CALL set_psi0soc_current(ryzen_access, uint32_t value);
+EXP int CALL set_max_gfxclk_freq(ryzen_access, uint32_t value);
+EXP int CALL set_min_gfxclk_freq(ryzen_access, uint32_t value);
+EXP int CALL set_max_socclk_freq(ryzen_access, uint32_t value);
+EXP int CALL set_min_socclk_freq(ryzen_access, uint32_t value);
+EXP int CALL set_max_fclk_freq(ryzen_access, uint32_t value);
+EXP int CALL set_min_fclk_freq(ryzen_access, uint32_t value);
+EXP int CALL set_max_vcn(ryzen_access, uint32_t value);
+EXP int CALL set_min_vcn(ryzen_access, uint32_t value);
+EXP int CALL set_max_lclk(ryzen_access, uint32_t value);
+EXP int CALL set_min_lclk(ryzen_access, uint32_t value);
 
 
 #ifdef __cplusplus
