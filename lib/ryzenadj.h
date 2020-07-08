658347926175a94f8a07c4d272d70de949d62f20
sbski <37050207+sbski@users.noreply.github.com>
2020-07-08 15:07:34 -0500

    Update ryzenadj.h

--- lib/ryzenadj.h
+++ lib/ryzenadj.h
@@ -74,6 +74,7 @@ EXP int CALL set_max_vcn(ryzen_access, uint32_t value);
 EXP int CALL set_min_vcn(ryzen_access, uint32_t value);
 EXP int CALL set_max_lclk(ryzen_access, uint32_t value);
 EXP int CALL set_min_lclk(ryzen_access, uint32_t value);
+EXP int CALL set_prochot_deassertion_ramp(ryzen_access ry, uint32_t value);
 
 
 #ifdef __cplusplus
