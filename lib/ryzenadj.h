75bc5a3b9124eb41bdf5dcd479fb39f924e0f845
sbski <37050207+sbski@users.noreply.github.com>
2020-08-08 09:02:23 -0500

    Updated Version Output
    It now will show "Version: v0.6.2" of Version: v6.2

--- lib/ryzenadj.h
+++ lib/ryzenadj.h
@@ -8,8 +8,9 @@
 extern "C" {
 #endif
 
-#define RYZENADJ_MAJOR_VER 0
-#define RYZENADJ_MINIOR_VER 7
+#define RYZENADJ_REVISION_VER 0
+#define RYZENADJ_MAJOR_VER 6
+#define RYZENADJ_MINIOR_VER 2
 
 enum ryzen_family {
         FAM_UNKNOWN = -1,
--- main.c
+++ main.c
@@ -102,7 +102,7 @@ int main(int argc, const char **argv)
 
 	struct argparse argparse;
 	argparse_init(&argparse, options, usage, 0);
-	argparse_describe(&argparse, "\n Ryzen Power Management adjust tool.", "\nWARNING: Use at your own risk!\nBy Jiaxun Yang <jiaxun.yang@flygoat.com>, Under LGPL.\nVersion: v" STRINGIFY(RYZENADJ_MAJOR_VER) "." STRINGIFY(RYZENADJ_MINIOR_VER));
+	argparse_describe(&argparse, "\n Ryzen Power Management adjust tool.", "\nWARNING: Use at your own risk!\nBy Jiaxun Yang <jiaxun.yang@flygoat.com>, Under LGPL.\nVersion: v" STRINGIFY(RYZENADJ_REVISION_VER) "." STRINGIFY(RYZENADJ_MAJOR_VER) "." STRINGIFY(RYZENADJ_MINIOR_VER));
 	argc = argparse_parse(&argparse, argc, argv);
 
 
