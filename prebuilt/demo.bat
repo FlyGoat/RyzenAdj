295077776fc9714eb78a64d084e9d407908541d1
unknown <Falcosc@users.noreply.github.com>
2021-03-06 22:35:09 +0100

    Change release structure to handle example scripts without manually moving files around

--- prebuilt/demo.bat
+++
@@ -1,2 +0,0 @@
-%~dp0\ryzenadj.exe --stapm-limit=40000 --fast-limit=45000 --slow-limit=45000 --tctl-temp=90
-pause
---
+++ scripts/demo.bat
@@ -0,0 +1,2 @@
+%~dp0\ryzenadj.exe --stapm-limit=40000 --fast-limit=45000 --slow-limit=45000 --tctl-temp=90
+pause
