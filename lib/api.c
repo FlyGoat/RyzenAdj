ad7f6205d14a55581a0a38c83cabc95745779c8b
Ew Meh <85849594+Pososaku@users.noreply.github.com>
2024-04-27 11:25:34 +0300

    Update api.c
    Per-core curve optimizer support for vangogh

--- lib/api.c
+++ lib/api.c
@@ -1283,6 +1283,9 @@ EXP int CALL set_coper(ryzen_access ry, uint32_t value) {
 		_do_adjust(0x54);
 	case FAM_REMBRANDT:
 	case FAM_PHOENIX:
+	case FAM_VANGOGH:  
+		_do_adjust(0x4b);  
+		break;
 	case FAM_HAWKPOINT:
 		_do_adjust(0x4B);
 		break;
