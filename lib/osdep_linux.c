c01e551491a888cd9c796a8ef38f325c955288e5
Alexander Heinrich <alxhnr@users.noreply.github.com>
2025-05-07 01:21:45 +0200

    Remove old, incomplete ryzen_smu backend

--- lib/osdep_linux.c
+++ lib/osdep_linux.c
@@ -6,11 +6,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
-#include <errno.h>
+
 #include "nb_smu_ops.h"
 
 bool mem_obj_obj = true;
-int pm_table_fd = 0;
 void *phy_map = MAP_FAILED;
 
 pci_obj_t init_pci_obj(){
@@ -51,38 +50,12 @@ void smn_reg_write(nb_t nb, u32 addr, u32 data)
 mem_obj_t init_mem_obj(uintptr_t physAddr)
 {
 	int dev_mem_fd; 
-	int dev_mem_errno, mmap_errno;
 
 	//It is to complicated to check PAT, CONFIG_NONPROMISC_DEVMEM, CONFIG_STRICT_DEVMEM or other dependencies, just try to open /dev/mem
 	dev_mem_fd = open("/dev/mem", O_RDONLY);
-	dev_mem_errno = errno;
 	if (dev_mem_fd > 0){
 		phy_map = mmap(NULL, 0x1000, PROT_READ, MAP_SHARED, dev_mem_fd, (long)physAddr);
-		mmap_errno = errno;
 		close(dev_mem_fd);
-	}
-
-	if(phy_map == MAP_FAILED){
-		//only complain about mmap errors if we don't have access to alternative smu driver
-		pm_table_fd = open("/sys/kernel/ryzen_smu_drv/pm_table", O_RDONLY);
-		if (pm_table_fd < 0) {
-			printf("failed to get /sys/kernel/ryzen_smu_drv/pm_table: %s\n", strerror(errno));
-
-			//show either fd error or mmap error, depending on which was the problem
-			if(dev_mem_errno) {
-				printf("failed to get /dev/mem: %s\n", strerror(dev_mem_errno));
-			} else {
-				printf("failed to map /dev/mem: %s\n", strerror(mmap_errno));
-			}
-
-			if(mmap_errno == EPERM || dev_mem_fd < 0) {
-				//we are already superuser if memory access is requested because we did successfully do the other smu calls.
-				//missing /dev/mem or missing permissions can only mean memory access policy
-				printf("If you don't want to change your memory access policy, you need a kernel module for this task.\n");
-				printf("We do support usage of this kernel module https://gitlab.com/leogx9r/ryzen_smu\n");
-			}
-			return NULL;
-		}
 	}
 
 	return &mem_obj_obj;
@@ -93,26 +66,12 @@ void free_mem_obj(mem_obj_t obj)
 	if(phy_map != MAP_FAILED){
 		munmap(phy_map, 0x1000);
 	}
-	if(pm_table_fd > 0) {
-		close(pm_table_fd);
-	}
 	return;
 }
 
 int copy_pm_table(nb_t nb, void *buffer, size_t size)
 {
-        (void)nb;
-	int read_size;
-
-	if(pm_table_fd > 0){
-		lseek(pm_table_fd, 0, SEEK_SET);
-		read_size = read(pm_table_fd, buffer, size);
-		if(read_size == -1) {
-			printf("failed to get pm_table from ryzen_smu kernel module: %s\n", strerror(errno));
-			return -1;
-		}
-		return 0;
-	}
+	(void)nb;
 
 	if(phy_map != MAP_FAILED){
 		memcpy(buffer, phy_map, size);
@@ -125,14 +84,10 @@ int copy_pm_table(nb_t nb, void *buffer, size_t size)
 
 int compare_pm_table(void *buffer, size_t size)
 {
-	if(pm_table_fd > 0){
-		//we can not compare to physial pm table location because we don't have direct memory access via SMU driver
-		return 0;
-	}
 	return memcmp(buffer, phy_map, size);
 }
 
 bool is_using_smu_driver()
 {
-	return pm_table_fd > 0;
+	return false;
 }
